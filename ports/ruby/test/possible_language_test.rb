c42193c35b0737e69539da2f8c9fe04d3ad3101b
Matt Sanford <matt@mzsanford.com>
2011-12-11 12:53:56 -0800

    Minor space cleanup

--- ports/ruby/test/possible_language_test.rb
+++ ports/ruby/test/possible_language_test.rb
@@ -10,7 +10,6 @@ class PossibleLanguageTest < Test::Unit::TestCase
     @possible_language.instance_variable_set(:@language, @language)
     @possible_language.instance_variable_set(:@score, 1.1)
     @possible_language.instance_variable_set(:@raw_score, 100)
-    # TODO: Setup values via set_ivar(?) for reading
   end
 
   def test_language_read_only
@@ -20,7 +19,7 @@ class PossibleLanguageTest < Test::Unit::TestCase
     end
     assert_equal(@language, @possible_language.language)
   end
-  
+
   def test_score_read_only
     assert_equal(1.1, @possible_language.score)
     assert_raise(NoMethodError) do
@@ -28,7 +27,7 @@ class PossibleLanguageTest < Test::Unit::TestCase
     end
     assert_equal(1.1, @possible_language.score)
   end
-  
+
   def test_raw_score_read_only
     assert_equal(100, @possible_language.raw_score)
     assert_raise(NoMethodError) do
